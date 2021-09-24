07cf27fdf226f206a5953d3566de765632d91e9c
Micha Sende <sende@lakeside-labs.com>
2021-09-24 09:25:23 +0200

    use correct git tag for czmqX

--- cmake/dependencies/build-czmq.cmake
+++ cmake/dependencies/build-czmq.cmake
@@ -2,15 +2,15 @@
 swarmio_build_component_start(czmq "4.1.1" libzmq)
 
 # Build czmq
-ExternalProject_Add(czmq 
+ExternalProject_Add(czmq
     GIT_REPOSITORY "https://github.com/cpswarm/czmq.git"
-    GIT_TAG "e305dc2136635cfb38a05e090580a833da54de28"
+    GIT_TAG "c288429c286d44b4636d871f3244b6011ad81173"
     PREFIX "czmq-${SWARMIO_TARGET_ARCHITECTURE}"
     BUILD_COMMAND ${SWARMIO_TARGET_BUILD_COMMAND}
     INSTALL_COMMAND ${SWARMIO_TARGET_INSTALL_COMMAND}
     INSTALL_DIR ${CZMQ_ROOT}
     LIST_SEPARATOR "|"
-    CMAKE_ARGS  
+    CMAKE_ARGS
         ${CZMQ_CMAKE_ARGS}
 )
 
