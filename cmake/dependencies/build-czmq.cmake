d6a2a19e40064708e4ed064a7c55eb5bd690b4cf
Micha Sende <sende@lakeside-labs.com>
2021-09-24 08:30:09 +0200

    use modified czmq in order to enable wwan interfaces

--- cmake/dependencies/build-czmq.cmake
+++ cmake/dependencies/build-czmq.cmake
@@ -3,7 +3,7 @@ swarmio_build_component_start(czmq "4.1.1" libzmq)
 
 # Build czmq
 ExternalProject_Add(czmq 
-    GIT_REPOSITORY "https://github.com/zeromq/czmq.git"
+    GIT_REPOSITORY "https://github.com/cpswarm/czmq.git"
     GIT_TAG "e305dc2136635cfb38a05e090580a833da54de28"
     PREFIX "czmq-${SWARMIO_TARGET_ARCHITECTURE}"
     BUILD_COMMAND ${SWARMIO_TARGET_BUILD_COMMAND}
